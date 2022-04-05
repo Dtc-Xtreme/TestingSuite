32ff1d7c0e590291c6c0b22b278315e11e00a2c1
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2022-04-05 14:44:32 +0200

    final after tests

--- TestingJSONParsers/Program.cs
+++ TestingJSONParsers/Program.cs
@@ -12,12 +12,14 @@ namespace TestingSuite
         {
             var summary1 = BenchmarkRunner.Run(typeof(SerializeMock0Test));
             var summary2 = BenchmarkRunner.Run(typeof(SerializeMock1Test));
+
             ////var summary3 = BenchmarkRunner.Run(typeof(SerializeMock2Test));
             ////var summary4 = BenchmarkRunner.Run(typeof(SerializeMock3Test));
             ////var summary5 = BenchmarkRunner.Run(typeof(SerializeMock4Test));
 
             var summary6 = BenchmarkRunner.Run(typeof(DeserializeMock0Test));
             var summary7 = BenchmarkRunner.Run(typeof(DeserializeMock1Test));
+
             ////var summary8 = BenchmarkRunner.Run(typeof(DeserializeMock2Test));
             ////var summary9 = BenchmarkRunner.Run(typeof(DeserializeMock3Test));
             ////var summary10 = BenchmarkRunner.Run(typeof(DeserializeMock4Test));
