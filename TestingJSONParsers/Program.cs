136a18c9e48961986c193ff6cdd9045fcf81a1fa
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2022-04-14 18:27:47 +0200

    context

--- TestingJSONParsers/Program.cs
+++ TestingJSONParsers/Program.cs
@@ -10,25 +10,25 @@ namespace TestingSuite
     {
         public static void Main(string[] args)
         {
-            var summary1 = BenchmarkRunner.Run(typeof(SerializeMock0Test));
-            var summary2 = BenchmarkRunner.Run(typeof(SerializeMock1Test));
+            //var summary1 = BenchmarkRunner.Run(typeof(SerializeMock0Test));
+            //var summary2 = BenchmarkRunner.Run(typeof(SerializeMock1Test));
 
             ////var summary3 = BenchmarkRunner.Run(typeof(SerializeMock2Test));
             ////var summary4 = BenchmarkRunner.Run(typeof(SerializeMock3Test));
             ////var summary5 = BenchmarkRunner.Run(typeof(SerializeMock4Test));
 
-            var summary6 = BenchmarkRunner.Run(typeof(DeserializeMock0Test));
-            var summary7 = BenchmarkRunner.Run(typeof(DeserializeMock1Test));
+            // var summary6 = BenchmarkRunner.Run(typeof(DeserializeMock0Test));
+            //var summary7 = BenchmarkRunner.Run(typeof(DeserializeMock1Test));
 
             ////var summary8 = BenchmarkRunner.Run(typeof(DeserializeMock2Test));
             ////var summary9 = BenchmarkRunner.Run(typeof(DeserializeMock3Test));
             ////var summary10 = BenchmarkRunner.Run(typeof(DeserializeMock4Test));
 
-            var summary11 = BenchmarkRunner.Run(typeof(ArrayTest));
+            //var summary11 = BenchmarkRunner.Run(typeof(ArrayTest));
 
             var summary12 = BenchmarkRunner.Run(typeof(JsonSerializerContextTest));
 
-            var summary13 = BenchmarkRunner.Run(typeof(SpanJsonUTfTest));
+            //var summary13 = BenchmarkRunner.Run(typeof(SpanJsonUTfTest));
 
         }
     }
--- TestingJSONParsers/Tests/JsonSerializerContextTest.cs
+++ TestingJSONParsers/Tests/JsonSerializerContextTest.cs
@@ -23,21 +23,19 @@ namespace TestingSuite.Tests
     {
         private readonly List<Mock1>? dataSingleCompact;
         private readonly List<Mock1>? dataMulti1000Compact;
+        private readonly List<Mock1>? dataMulti10000Compact;
+        private readonly List<Mock1>? dataMulti100000Compact;
 
         public JsonSerializerContextTest()
         {
             this.dataSingleCompact = Tools.CreateMockObjectsX<Mock1>(1, 'C', false);
             this.dataMulti1000Compact = Tools.CreateMockObjectsX<Mock1>(1000, 'C', false);
+            this.dataMulti10000Compact = Tools.CreateMockObjectsX<Mock1>(10000, 'C', false);
+            this.dataMulti100000Compact = Tools.CreateMockObjectsX<Mock1>(100000, 'C', false);
         }
-
 
         // Serializing //
-        // System.Text.Json //
-        [Benchmark]
-        public string SystemTextJsonSerializeSingleCompact()
-        {
-            return System.Text.Json.JsonSerializer.Serialize(dataSingleCompact);
-        }
+        // System.Text.Json met context//
 
         [Benchmark]
         public string SystemTextJsonSerializeSingleCompactWithContext()
@@ -46,15 +44,21 @@ namespace TestingSuite.Tests
         }
 
         [Benchmark]
-        public string SystemTextJsonSerializeMulti1000Compact()
-        {
-            return System.Text.Json.JsonSerializer.Serialize(dataMulti1000Compact);
-        }
-
-        [Benchmark]
         public string SystemTextJsonSerializeMulti1000CompactWithContext()
         {
             return System.Text.Json.JsonSerializer.Serialize(dataMulti1000Compact, Mock1JsonSerializerContext.Default.ListMock1);
+        }
+
+        [Benchmark]
+        public string SystemTextJsonSerializeMulti10000CompactWithContext()
+        {
+            return System.Text.Json.JsonSerializer.Serialize(dataMulti10000Compact, Mock1JsonSerializerContext.Default.ListMock1);
+        }
+
+        [Benchmark]
+        public string SystemTextJsonSerializeMulti100000CompactWithContext()
+        {
+            return System.Text.Json.JsonSerializer.Serialize(dataMulti100000Compact, Mock1JsonSerializerContext.Default.ListMock1);
         }
     }
 }
